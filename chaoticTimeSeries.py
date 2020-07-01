281117f01616fd6114dad35fc2e74f64557682ee
unknown <juandavid.hernandez@utp.edu.co>
2020-07-01 17:13:55 -0500

    Generador de series de tiempo caoticas

--- chaoticTimeSeries.py
+++ chaoticTimeSeries.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-   
+# -- coding: utf-8 --   
 """
 Created on Tue Jun 30 11:51:26 2020
 
@@ -37,4 +37,165 @@ def chua(x=0, y=0, z=1, **kwargs):
     # y_out = x + z
     # z_out = y
 
+    return x_out, y_out, z_out
+
+def lorenz(x=0, y=0, z=0, **kwargs):
+    """
+    Calculate the next coordinate X, Y, Z for 3rd-order Lorenz system
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    kwargs : float
+        beta, rho and sigma - are Lorenz system parameters
+
+    """
+    # Default Lorenz parameters:
+    sigma = kwargs.get('sigma', 10)
+    beta = kwargs.get('beta', 8/3)
+    rho = kwargs.get('rho', 28)
+
+    # Next step coordinates:
+    x_out = sigma * (y - x)
+    y_out = rho*x - y - x*z
+    z_out = x*y - beta*z
+
+    return x_out, y_out, z_out
+
+def lotka_volterra(x=0, y=0, z=0):
+    """
+    Calculate the next coordinate X, Y, Z for Lotka–Volterra
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    """
+    # Next step coordinates:
+    x_out = x*(1 - x - 9*y)
+    y_out = -y*(1 - 6*x - y + 9*z)
+    z_out = z*(1 - 3*x - z)
+    return x_out, y_out, z_out
+
+
+def duffing(x=0, y=0, z=0, **kwargs):
+    """
+    Calculate the next coordinate X, Y, Z for Duffing map.
+    It is 2nd order attractor (Z coordinate = 1)
+
+    Duffing map:
+    Eq. 1:
+        dx/dt = y
+        dy/dt = -a*y - x**3 + b * cos(z)
+        dz/dt = 1
+    where a = 0.1 and b = 11 (default parameters)
+
+    Eq. 2:
+        dx/dt = y
+        dy/dt = a*y - y**3 - b*x
+        dz/dt = 1
+    where a = 2.75 and b = 0.2 (default parameters)
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    kwargs : float
+        a and b - are Duffing system parameters
+    """
+    # Default parameters:
+    # a = kwargs.get('a', 2.75)
+    # b = kwargs.get('b', 0.2)
+    a = kwargs.get('a', 0.1)
+    b = kwargs.get('b', 11)
+
+    # Next step coordinates:
+    x_out = y
+    y_out = -a*y - x**3 + b*np.cos(z)
+    # y_out = a*y - y**3 - b*x
+    z_out = 1
+    return x_out, y_out, z_out
+
+
+
+def nose_hoover(x=0, y=0, z=0):
+    """
+    Calculate the next coordinate X, Y, Z for 3rd-order Nose-Hoover
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    """
+    # Next step coordinates:
+    x_out = y
+    y_out = y * z - x
+    z_out = 1 - y * y
+
+    return x_out, y_out, z_out
+
+
+def rikitake(x=0, y=0, z=0, **kwargs):
+    """
+    Calculate the next coordinate X, Y, Z for 3rd-order Rikitake system
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    kwargs : float
+        mu, a - are Rikitake system parameters
+
+    """
+    # Default Rikitake parameters:
+    a = kwargs.get('a', 5)
+    mu = kwargs.get('mu', 2)
+
+    # Next step coordinates:
+    x_out = -mu * x + z * y
+    y_out = -mu * y + x * (z - a)
+    z_out = 1 - x * y
+
+    return x_out, y_out, z_out
+
+
+def rossler(x=0, y=0, z=0, **kwargs):
+    """
+    Calculate the next coordinate X, Y, Z for 3rd-order Rossler system
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    kwargs : float
+        a, b and c - are Rossler system parameters
+
+    """
+    # Default Rossler parameters:
+    a = kwargs.get('a', 0.2)
+    b = kwargs.get('b', 0.2)
+    c = kwargs.get('c', 5.7)
+
+    # Next step coordinates:
+    x_out = -(y + z)
+    y_out = x + a * y
+    z_out = b + z * (x - c)
+
+    return x_out, y_out, z_out
+
+
+def wang(x=0, y=0, z=0):
+    """
+    Calculate the next coordinate X, Y, Z for 3rd-order Wang Attractor
+
+    Parameters
+    ----------
+    x, y, z : float
+        Input coordinates Z, Y, Z respectively
+    """
+    # Next step coordinates:
+    x_out = x - y*z
+    y_out = x - y + x*z
+    z_out = -3*z + x*y
     return x_out, y_out, z_out
--- test.py
+++ test.py
@@ -4,7 +4,9 @@ Created on Tue Jun 30 14:33:28 2020
 
 @author: Juan David
 """
+attr = ["chua","lorenz","duffing","nose_hoover","rikitake","rossler","wang"]
+import TimeSeriesGenerator as tsg
 
-import chaoticTimeSeries as CTS
-
-CTS.chua(0,0,0)
+for sys in attr:
+    x, y, z = tsg.chaoticSystem(samples=10000,systemType=sys)
+    
